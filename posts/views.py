1693f4e1076eebc0c2f99a714c3f898f603a6ccd
Jules Ezeh <julesezeh23@gmail.com>
2022-11-11 14:33:02 +0100

    Permissions added to api views and rest_framework urls added in the base urls.py file

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework import generics, permissions
 # Create your views here.
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
